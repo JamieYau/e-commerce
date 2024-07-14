056ca7787b72ca1bc71e359b17a375094090a6c6
JamieYau <jamie.yau2002@me.com>
2024-07-14 22:57:48 +0100

    Add change username form on a protected settings route

---
+++ app/settings/page.tsx
@@ -0,0 +1,25 @@
+import { auth } from "@/auth";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import SettingsForm from "./SettingsForm";
+
+export const metadata: Metadata = {
+  title: "settings",
+};
+
+export default async function Page() {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/api/auth/signin?callbackUrl=/settings");
+  }
+  return (
+    <main>
+      <section>
+        <h1>Settings Page</h1>
+        <SettingsForm user={user} />
+      </section>
+    </main>
+  );
+}
