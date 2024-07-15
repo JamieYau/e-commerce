9aa4eb4b732117cc4adf93d1e765d78e448ba381
JamieYau <jamie.yau2002@me.com>
2024-07-15 21:53:06 +0100

    Add Resend to auth providers

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 import db from "./db/db";
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
+import Resend from "next-auth/providers/resend";
 import {
   accounts,
   authenticators,
@@ -31,5 +32,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session;
     },
   },
-  providers: [Google, Github],
+  providers: [
+    Google,
+    Github,
+    Resend({
+      from: "no-reply@techwave.jamieyau.com",
+    }),
+  ],
 });
