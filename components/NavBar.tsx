2f635338bab51e09ca673b5ce9851ce390abda04
JamieYau <jamie.yau2002@me.com>
2024-07-17 20:41:49 +0100

    Add branding to site

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,15 +5,18 @@ import { Button } from "./ui/button";
 import UserButton from "./UserButton";
 import { signIn, useSession } from "next-auth/react";
 import { Skeleton } from "./ui/skeleton";
+import Image from "next/image";
 
 export default function NavBar() {
   const session = useSession();
   const user = session.data?.user;
 
   return (
-    <header className="shadow-sm z-10">
+    <header className="shadow-sm z-10 p-2">
       <nav className="flex w-full max-w-7xl items-center justify-between gap-3 mx-auto h-14">
-        <Link href="/">E-Commerce</Link>
+        <Link href="/">
+          <Image src="/images/logo-black.png" alt="techwave" width={200} height={39} priority/>
+        </Link>
         {user && <UserButton user={user} />}
         {session.status === "loading" && (
           <Skeleton className="rounded-full h-10 w-10" />
