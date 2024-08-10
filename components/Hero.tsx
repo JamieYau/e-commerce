2581dce2370a4609fbb9576ee15f6e2b9ad9ed11
JamieYau <jamie.yau2002@me.com>
2024-08-10 04:27:16 +0100

    Fix being Browse products link being unclickable on hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,14 +5,14 @@ import { buttonVariants } from "./ui/button";
 export default function Hero() {
   return (
     <section>
-      <div className="relative -z-20 m-auto flex aspect-[5/2] min-h-[450px] w-full items-center bg-zinc-100">
+      <div className="relative m-auto flex aspect-[5/2] min-h-[450px] w-full items-center bg-zinc-100">
         <Image
           src={"/images/hero-1.png"}
           alt="hero"
           fill
-          className="-z-10 object-cover"
+          className="absolute left-0 top-0 h-full w-full object-cover"
         />
-        <div className="ml-8 mr-auto sm:ml-28">
+        <div className="relative z-10 ml-8 mr-auto sm:ml-28">
           <h1 className="max-w-72 text-5xl font-bold">Shop at TechWave</h1>
           <p className="my-4 text-lg font-medium">Explore best tech products</p>
           <p className="mb-10 max-w-52 text-sm font-semibold text-muted-foreground">
