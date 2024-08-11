6cda5ae96363b22751adb7cdcf8e72dfb6322d96
JamieYau <jamie.yau2002@me.com>
2024-08-11 19:17:44 +0100

    Add search query to url search params on submit

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface SearchBarProps {
   setIsSearchVisible: React.Dispatch<React.SetStateAction<boolean>>;
@@ -15,6 +16,8 @@ export default function SearchBar({
   setIsSearchVisible,
   isVisible,
 }: SearchBarProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -23,20 +26,31 @@ export default function SearchBar({
     }
   }, [isVisible]);
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Implement search functionality here
-    console.log("Search submitted:", inputRef.current?.value);
-  };
+    const handleSubmit = (event: React.FormEvent) => {
+      event.preventDefault();
+
+      const query = inputRef.current?.value || "";
+      const params = new URLSearchParams(searchParams);
+
+      if (query) {
+        params.set("q", query);
+      } else {
+        params.delete("q");
+      }
+      router.push(`/products?${params}`);
+    };
 
   return (
     <div
       className={cn(
-        `absolute left-0 top-full hidden w-full bg-background p-4 sm:p-0 sm:static sm:flex sm:max-w-80 shadow-sm sm:shadow-none`,
+        `absolute left-0 top-full hidden w-full bg-background p-4 shadow-sm sm:static sm:flex sm:max-w-80 sm:p-0 sm:shadow-none`,
         isVisible && "flex",
       )}
     >
-      <form onSubmit={handleSubmit} className={cn(`flex w-full items-center gap-1`)}>
+      <form
+        onSubmit={handleSubmit}
+        className={cn(`flex w-full items-center gap-1`)}
+      >
         <div className="flex w-full items-center gap-2 rounded-xl border p-2">
           <Search className="h-5 w-5" aria-hidden="true" />
           <Input
